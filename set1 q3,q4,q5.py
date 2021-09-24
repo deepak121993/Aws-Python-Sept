9a4ce4514ba35819937a3cb4df17f6eae3353f96
NandiniUp <90875980+NandiniUp@users.noreply.github.com>
2021-09-24 12:56:03 -0400

    Delete set1 q3,q4,q5.py

--- set1 q3,q4,q5.py
+++
@@ -1,8 +0,0 @@
-#.Write a for loop that prints all elements of a list and their position in the list.
-a = [4,7,3,2,5,9]
-for i in range(0,len(a)):
-    print("the elemets of a and their position is:",a[i],i)
-
-mystring=input("enter your name") 
-print("the even index of the string",mystring[0::2]) #even index question 4
-print("the reverse order of the string is",mystring[::-1]) #reverse string question 5
